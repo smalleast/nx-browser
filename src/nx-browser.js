7d16b7a70a4eb064d6c9f91cb047cf29c1a6bffe
dcpai <huangxiaodong@dcpai.cn>
2017-08-16 18:02:46 +0800

    rebuild nx-browser u==>UA

--- src/nx-browser.js
+++ src/nx-browser.js
@@ -25,19 +25,19 @@
     statics: {
       init: function () {
         nx.mix(this, {
-          trident: u.indexOf('Trident') > -1, //IE内核
-          presto: u.indexOf('Presto') > -1, //opera内核
-          webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
-          gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1,//火狐内核
-          mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
-          ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
-          android: u.indexOf('Android') > -1 || u.indexOf('Adr') > -1, //android终端
-          iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
-          iPad: u.indexOf('iPad') > -1, //是否iPad
-          webApp: u.indexOf('Safari') == -1, //是否web应该程序，没有头部与底部
-          weixin: u.indexOf('MicroMessenger') > -1, //是否微信 （2015-01-22新增）
-          qq: u.match(/\sQQ/i) == " qq", //是否QQ
-          ios8: u.indexOf('OS 8') > -1//判断是否ios8
+          trident: UA.indexOf('Trident') > -1, //IE内核
+          presto: UA.indexOf('Presto') > -1, //opera内核
+          webKit: UA.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+          gecko: UA.indexOf('Gecko') > -1 && UA.indexOf('KHTML') == -1,//火狐内核
+          mobile: !!UA.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+          ios: !!UA.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+          android: UA.indexOf('Android') > -1 || UA.indexOf('Adr') > -1, //android终端
+          iPhone: UA.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+          iPad: UA.indexOf('iPad') > -1, //是否iPad
+          webApp: UA.indexOf('Safari') == -1, //是否web应该程序，没有头部与底部
+          weixin: UA.indexOf('MicroMessenger') > -1, //是否微信 （2015-01-22新增）
+          qq: UA.match(/\sQQ/i) == " qq", //是否QQ
+          ios8: UA.indexOf('OS 8') > -1//判断是否ios8
         });
       },
       engine: function () {
